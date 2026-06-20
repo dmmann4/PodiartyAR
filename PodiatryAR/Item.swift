3f7b4cf974c88d64e79df1272e85d7fb53b6ac8f
David Mann <manndm4@gmail.com>
2026-06-19 22:21:27 -0400

    Initial Commit

---
+++ PodiatryAR/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PodiatryAR
+//
+//  Created by Mann Fam on 6/19/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ PodiatryARTests/PodiatryARTests.swift
@@ -0,0 +1,19 @@
+//
+//  PodiatryARTests.swift
+//  PodiatryARTests
+//
+//  Created by Mann Fam on 6/19/26.
+//
+
+import Testing
+@testable import PodiatryAR
+
+struct PodiatryARTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
